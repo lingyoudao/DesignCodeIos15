06e09d04d4440d7c5bd5de7f6fb7dbf9352a8664
lingyoudao <lingyoudao@hotmail.com>
2024-12-29 10:44:02 +0800

    Initial Commit

---
+++ DesignCodeIos15/DesignCodeIos15App.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeIos15App.swift
+//  DesignCodeIos15
+//
+//  Created by 凌有道 on 2024/12/29.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeIos15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
