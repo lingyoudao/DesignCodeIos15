b6609883d9b2777fa0f32a3e7584aca22c61537e
lingyoudao <lingyoudao@hotmail.com>
2025-03-16 11:35:56 +0800

    s5---shape, mask, stroke and blend mode

--- DesignCodeIos15/ContentView.swift
+++ DesignCodeIos15/ContentView.swift
@@ -17,6 +17,8 @@ struct ContentView: View {
                     .aspectRatio(contentMode: .fit)
                     .frame(width: 26.0, height: 26.0)
                     .cornerRadius(/*@START_MENU_TOKEN@*/20.0/*@END_MENU_TOKEN@*/)
+                    .padding(9)
+                    .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 20, style: .continuous))
                     
                 Text("SwiftUI for iOS 15")
                     .font(.largeTitle)
@@ -34,13 +36,20 @@ struct ContentView: View {
                     .lineLimit(2)
                     .frame(maxWidth: .infinity, alignment: .leading)
                     .foregroundColor(.secondary)
+                    
             }
             .padding(.all, 20.0)
             .padding(.vertical, 20)
             .frame(height: 350.0)
             .background(.ultraThinMaterial)
-            .cornerRadius(/*@START_MENU_TOKEN@*/30.0/*@END_MENU_TOKEN@*/)
+            //.cornerRadius(/*@START_MENU_TOKEN@*/30.0/*@END_MENU_TOKEN@*/)
+            .mask(RoundedRectangle(cornerRadius: 30, style: .continuous))
             .shadow(color: Color("Shadow").opacity(0.3), radius: 10, x: 0, y: 10)
+            .overlay(
+                RoundedRectangle(cornerRadius: 30, style: .continuous)
+                    .stroke(.linearGradient(colors: [.white.opacity(0.3), .black.opacity(0.1)], startPoint: .top, endPoint: .bottom))
+                    .blendMode(.overlay)
+            )
             .padding(.horizontal, 20)
             .background(Image("Blob 1").offset(x: 250, y:   -100))
             .overlay(
