e9122c1d262f6b1694e540b68f33b94902f1906b
lingyoudao <lingyoudao@hotmail.com>
2025-03-16 10:26:41 +0800

    s4--background 2 + overlay + primary + linear gradient

--- DesignCodeIos15/ContentView.swift
+++ DesignCodeIos15/ContentView.swift
@@ -18,15 +18,16 @@ struct ContentView: View {
                     .frame(width: 26.0, height: 26.0)
                     .cornerRadius(/*@START_MENU_TOKEN@*/20.0/*@END_MENU_TOKEN@*/)
                     
-                Text("Hello, world!")
+                Text("SwiftUI for iOS 15")
                     .font(.largeTitle)
                     .fontWeight(.bold)
-                
+                    .foregroundStyle(.linearGradient(colors: [.primary, .primary.opacity(0.5)], startPoint: .topLeading, endPoint: .bottomTrailing))
+                //.primary颜色，在light模式下黑，dark mode下黑
                 
                 Text(/*@START_MENU_TOKEN@*/"20 sections - 3 hours"/*@END_MENU_TOKEN@*/)
                     .font(.footnote)
                     .fontWeight(.semibold)
-                    .foregroundColor(.secondary)
+                    .foregroundStyle(.secondary)
                 Text("Builld an iOS app for iOS15 with custom layouts, animations and ...")
                     .font(.footnote)
                     .multilineTextAlignment(.leading)
@@ -37,10 +38,18 @@ struct ContentView: View {
             .padding(.all, 20.0)
             .padding(.vertical, 20)
             .frame(height: 350.0)
-            .background(Color("Background"))
+            .background(.ultraThinMaterial)
             .cornerRadius(/*@START_MENU_TOKEN@*/30.0/*@END_MENU_TOKEN@*/)
             .shadow(color: Color("Shadow").opacity(0.3), radius: 10, x: 0, y: 10)
             .padding(.horizontal, 20)
+            .background(Image("Blob 1").offset(x: 250, y:   -100))
+            .overlay(
+                Image("Illustration 5")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(height:230)
+                    .offset(x: 32, y: -80)
+            )
             
         
     }
