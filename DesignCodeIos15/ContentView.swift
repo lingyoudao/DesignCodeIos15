bb2f0733aaf46f54e8c1b07248a552ff172fd9a0
lingyoudao <lingyoudao@hotmail.com>
2025-03-16 08:56:56 +0800

    s3

--- DesignCodeIos15/ContentView.swift
+++ DesignCodeIos15/ContentView.swift
@@ -9,13 +9,48 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+            VStack(alignment: .leading, spacing: 8.0) {
+                Spacer()
+                Image("Logo 2")
+                    .resizable(resizingMode: .stretch)
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 26.0, height: 26.0)
+                    .cornerRadius(/*@START_MENU_TOKEN@*/20.0/*@END_MENU_TOKEN@*/)
+                    
+                Text("Hello, world!")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                
+                
+                Text(/*@START_MENU_TOKEN@*/"20 sections - 3 hours"/*@END_MENU_TOKEN@*/)
+                    .font(.footnote)
+                    .fontWeight(.semibold)
+                    .foregroundColor(.secondary)
+                Text("Builld an iOS app for iOS15 with custom layouts, animations and ...")
+                    .font(.footnote)
+                    .multilineTextAlignment(.leading)
+                    .lineLimit(2)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .foregroundColor(.secondary)
+            }
+            .padding(.all, 20.0)
+            .padding(.vertical, 20)
+            .frame(height: 350.0)
+            .background(Color("Background"))
+            .cornerRadius(/*@START_MENU_TOKEN@*/30.0/*@END_MENU_TOKEN@*/)
+            .shadow(color: Color("Shadow").opacity(0.3), radius: 10, x: 0, y: 10)
+            .padding(.horizontal, 20)
+            
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+                .previewDevice("iPhone 12")
+        }
     }
 }
