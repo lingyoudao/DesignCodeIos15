5df4a0603baf9b04d941bd8073cb3cf6aed320e0
lingyoudao <lingyoudao@hotmail.com>
2025-03-23 08:34:25 +0800

    6-样式复用StrokeStyle和颜色主题colorScheme

--- DesignCodeIos15/ContentView.swift
+++ DesignCodeIos15/ContentView.swift
@@ -16,15 +16,16 @@ struct ContentView: View {
                     .resizable(resizingMode: .stretch)
                     .aspectRatio(contentMode: .fit)
                     .frame(width: 26.0, height: 26.0)
-                    .cornerRadius(/*@START_MENU_TOKEN@*/20.0/*@END_MENU_TOKEN@*/)
+                    .cornerRadius(10)
                     .padding(9)
-                    .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 20, style: .continuous))
-                    
+                    .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 16, style: .continuous))
+                    .strokeStyle(cornerRadius: 16)
+//                    .modifier(StrokeStyle())
                 Text("SwiftUI for iOS 15")
                     .font(.largeTitle)
                     .fontWeight(.bold)
                     .foregroundStyle(.linearGradient(colors: [.primary, .primary.opacity(0.5)], startPoint: .topLeading, endPoint: .bottomTrailing))
-                //.primary颜色，在light模式下黑，dark mode下黑
+                //.primary颜色，在light模式下黑，dark mode下白
                 
                 Text(/*@START_MENU_TOKEN@*/"20 sections - 3 hours"/*@END_MENU_TOKEN@*/)
                     .font(.footnote)
@@ -45,11 +46,8 @@ struct ContentView: View {
             //.cornerRadius(/*@START_MENU_TOKEN@*/30.0/*@END_MENU_TOKEN@*/)
             .mask(RoundedRectangle(cornerRadius: 30, style: .continuous))
             .shadow(color: Color("Shadow").opacity(0.3), radius: 10, x: 0, y: 10)
-            .overlay(
-                RoundedRectangle(cornerRadius: 30, style: .continuous)
-                    .stroke(.linearGradient(colors: [.white.opacity(0.3), .black.opacity(0.1)], startPoint: .top, endPoint: .bottom))
-                    .blendMode(.overlay)
-            )
+            //.modifier(StrokeStyle())
+            .strokeStyle()
             .padding(.horizontal, 20)
             .background(Image("Blob 1").offset(x: 250, y:   -100))
             .overlay(
@@ -68,7 +66,12 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         Group {
             ContentView()
+                .preferredColorScheme(.light)
                 .previewDevice("iPhone 12")
+            ContentView()
+                .preferredColorScheme(.dark)
+                .previewDevice("iPhone 12")
+           
         }
     }
 }
