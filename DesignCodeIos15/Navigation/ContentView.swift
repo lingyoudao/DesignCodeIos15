bb22deb4b080384b140242ff8cf46473f8d7b369
lingyoudao <lingyoudao@hotmail.com>
2025-06-07 07:53:39 +0800

    23 - Safe Area Insect and NavigationBar Button

--- DesignCodeIos15/Navigation/ContentView.swift
+++ DesignCodeIos15/Navigation/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
         
         ZStack(alignment: .bottom){
                         
-            Group{
+           
                 switch selectedTab {
                 case .home:
                     HomeView()
@@ -24,12 +24,14 @@ struct ContentView: View {
                 case .library:
                     AccountView()
                 }
-            }
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
+           
 
            TabBar()
                 
-                }
+        }
+        .safeAreaInset(edge: .bottom) {
+            Color.clear.frame(height:44)
+        }
         
     }
 }
